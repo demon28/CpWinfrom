8d8818760f61ca452be6cd2c6393451705ca6c60
Near <274733956@qq.com>
2019-07-18 18:02:17 +0800

    10%

--- CpWinfrom/Rule/Teshuhao.cs
+++ CpWinfrom/Rule/Teshuhao.cs
@@ -172,7 +172,7 @@ namespace CpWinfrom.Rule
                     if (numberModels.Count(S => S.N1 == item.N1 && S.N2 == item.N2 && S.N3 == item.N3 && S.N4 == item.N4) == 0)
                     {
                         numberModels.Add(item);
-                    }
+                    }   
 
                 }
                 if (item.N3 == item.N4 + 1 || item.N3 == item.N1 + 1 || item.N2 == item.N2 + 1)
